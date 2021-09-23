2702fbb3aa266afdf5ed9473d6538f427437c219
andrutsuko <andrutsuko@gmail.com>
2021-09-22 19:40:04 -0500

    Esta es la primera subida de archivos al repositorio del BackEnd

---
+++ TiendaGenericaGrupo09/src/main/java/com/example/TiendaGenericaGrupo09/TiendaGenericaGrupo09Application.java
@@ -0,0 +1,13 @@
+package com.example.TiendaGenericaGrupo09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaGenericaGrupo09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaGenericaGrupo09Application.class, args);
+	}
+
+}
---
+++ TiendaGenericaGrupo09/src/test/java/com/example/TiendaGenericaGrupo09/TiendaGenericaGrupo09ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TiendaGenericaGrupo09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaGenericaGrupo09ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
